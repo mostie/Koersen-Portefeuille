233283a5bbb7fcf9bf983c5c29cc187dd4c1db36
Peter Mostmans <peter.mostmans@telenet.be>
2018-09-05 22:36:11 +0200

    Lijkt te lukken
    De slotkoers is niet juist, maar is eerder te wijten aan rariteit van de html pagina.  Tijdens het laden geeft ze de koers die door de python kode opgepikt wordt.  Deze is anders dan wanneer de pagina volledig geladen is.

--- koersen_bel20_s3.py
+++ koersen_bel20_s3.py
@@ -5,6 +5,7 @@ import codecs
 import xlsxwriter
 import openpyxl
 from openpyxl import load_workbook
+import time
 import datetime
 
 from datetime import datetime
@@ -15,27 +16,28 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 import boto3
 import csv
 
+
+'''
 # input : UU:MM => YYYY/MM/DD UU:00:00
 def convert_time_to_date_time(etime):
     etime_array = etime.split(":")
-    #print(etime_array[0])
+    print(etime_array)
     #print(etime_array[1])
     etime = etime_array[0] + ":" + "00"
-    #print(etime)
+    print(etime)
     etime += ':00'
+    print(etime)
     current_datetime = str(datetime.datetime.now())[0:10]
+    print(current_datetime)
     current_datetime = current_datetime + " " + etime
-    #print(current_datetime)
+    print(current_datetime)
     return current_datetime
 
-    #rcurrent_datetime.strftime('%x %X')
 
 def csv_writer(output, path):
 
     """
-
     Write output to a CSV file path
-
     """
 
     with open(path, "w") as csv_file:
@@ -50,81 +52,62 @@ def csv_writer(output, path):
 
             writer.writerow(line)
             
-
+'''
 
 def write_bel20_to_amazon_s3():
 
 
-    print('test')
-    
+    #print('=> begin write_bel20_to_amazon_s3')   
 
     # zorgt ervoor dat speciale karakters geprint worden : vb. Chinese karakters
     #sys.stdout = codecs.getwriter('utf8')(sys.stdout.buffer)
 
-
-
     html = requests.get('https://www.beursduivel.be/Koersen/Aandelen.aspx')
+    #print(type(html))
     doc = lxml.html.fromstring(html.content)
-
+    #print(type(doc))
     Issue_Default = doc.xpath('//div[@class="IssueDefault"]')[0]
+    #print(type(Issue_Default))
     titles = Issue_Default.xpath('.//td[@class="TitleCell DateTimeCell"]/a/text()')
+    #print(type(titles))
     lastprices = Issue_Default.xpath('.//td[@class="ValueCell"][1]/span/text()')
     highprices = Issue_Default.xpath('.//td[@class="ValueCell"][4]/span/text()')
     lowprices = Issue_Default.xpath('.//td[@class="ValueCell"][5]/span/text()')
     finals = Issue_Default.xpath('.//td[@class="ValueCell"][6]/span/text()')
-    #times = Issue_Default.xpath('.//td[@class="ValueCell"][7]/span/text()')
+    times = Issue_Default.xpath('.//td[@class="ValueCell"][7]/span/text()')
+
+    #print(times)
+    #print(finals)
 
 
-    output = []
-    #for info in zip(titles, lastprices, highprices, lowprices,finals,times):
-    for info in zip(titles, lastprices, highprices, lowprices,finals):        
-        resp = {}
-        resp['title'] = info[0]
-        resp['price'] = info[1]
-        resp['high'] = info[2]
-        resp['low'] = info[3]
-        resp['final'] = info[4]
-        #resp['time'] = convert_time_to_date_time(info[5])
-        output.append(resp)
-        #for var in enumerate(output):
-        #    print(var)
-
+    localtime = time.localtime(time.time())
+    #print("Local current time :", localtime.tm_year)
 
     row = 0
-
-    for j in output:
-            row +=1
-            col = 0
-            for k in j:
-                col += 1
-                print(j[k])
-                print(row)
-                print(col)
-
-    path = "d:\data\it\python\koersen.csv"
-
-    csv_writer(output, path)
-
-    # Create an S3 client
-    s3 = boto3.client('s3')
-
-    filename = "d:\data\it\python\koersen.csv"
-    bucket_name = 'mostie-algemeen'
-
-    # Uploads the given file using a managed uploader, which will split up large
-    # files automatically and upload parts in parallel.
-    s3.upload_file(filename, bucket_name, filename)    
-
-
+    #for info in zip(titles, lastprices, highprices, lowprices,finals,times):
+    csvfile = open('koersen.csv', 'w')
+    with csvfile:                        
+        myFields = ['title','price','high','low','final','time']
+        writer = csv.DictWriter(csvfile,fieldnames=myFields)
+        writer.writeheader()            
+        for info in zip(titles[1:], lastprices[1:], highprices[1:], lowprices[1:],finals[1:],times[1:]):        
+            resp = {}
+            resp['title'] = info[0]
+            resp['price'] = float(info[1].replace(',','.'))            
+            resp['high']  = float(info[2].replace(',','.'))            
+            resp['low']   = float(info[3].replace(',','.'))            
+            resp['final'] = float(info[4].replace(',','.'))            
+            resp['time']  = str(localtime.tm_year)+'/'+str(localtime.tm_mon) + '/'+str(localtime.tm_mday) +' '+ info[5]
+            writer.writerow(resp)
 
 if __name__ == '__main__':
+    '''
     scheduler = BlockingScheduler()
     scheduler.add_job(write_bel20_to_amazon_s3, 'interval', seconds=5)
     print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
-
     try:
         scheduler.start()
     except (KeyboardInterrupt, SystemExit):
         pass
-
-#write_bel20_to_amazon_s3()
+    '''
+write_bel20_to_amazon_s3()
---
+++ test2.py
@@ -0,0 +1,5 @@
+import csv
+import time
+
+localtime = time.localtime(time.time())
+print("Local current time :", localtime.tm_year)
