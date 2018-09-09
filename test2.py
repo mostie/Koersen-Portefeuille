59e96d398a1645997132a6d89cc4f0b88000437d
Peter Mostmans <peter.mostmans@telenet.be>
2018-09-09 18:39:29 +0200

    9 wijzigingen

--- test2.py
+++ test2.py
@@ -1,5 +1,23 @@
-import csv
-import time
+import pymysql
 
-localtime = time.localtime(time.time())
-print("Local current time :", localtime.tm_year)
+HOST = "pemotec.be"
+PORT = 3306
+USER = "most_pemotec"
+PASSWORD = "z9H5%R^pjPxM&ePA"
+DB = "pemotec_test"
+
+try:
+    connection = pymysql.Connect(host=HOST, port=PORT,
+                               user=USER, passwd=PASSWORD, db=DB)
+
+    dbhandler = connection.cursor()
+    dbhandler.execute("SELECT * from most")
+    result = dbhandler.fetchall()
+    for item in result:
+        print(item)
+
+except Exception as e:
+    print(e)
+
+finally:
+	connection.close()
